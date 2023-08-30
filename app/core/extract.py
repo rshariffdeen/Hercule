7ad4363b56d3010945fc6403e78464473ae5c6c1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-30 11:08:32 +0800

    refactoring

--- app/core/extract.py
+++ app/core/extract.py
@@ -1,5 +1,11 @@
 import filetype
-from app.core import utilities, emitter
+import os
+import re
+from os.path import join
+from git import Repo
+from app.core import utilities, emitter, values
+from app.tools import archives
+
 
 
 def extract_file_types(dir_path):
@@ -37,3 +43,134 @@ def extract_compiled_python(dir_path):
         if "python 3" in kind and "byte-compiled" in kind:
             compiled_python3_list.append(f_p)
     return compiled_python2_list, compiled_python3_list
+
+
+def extract_source(source_url, github_page, dir_src, pkg_version):
+    emitter.sub_title("Extracting Source")
+    if not os.path.isdir(dir_src):
+        if source_url:
+            emitter.sub_sub_title("downloading from URL")
+            release_file = os.path.dirname(dir_src) + "/src-archive"
+            utilities.download_file(source_url, release_file)
+            file_type = filetype.guess(release_file)
+            if not file_type:
+                utilities.error_exit(f"unknown release file type {release_file}")
+            file_ext = file_type.extension
+            archives.decompress_archive(release_file, file_ext, dir_src)
+        elif github_page:
+            emitter.sub_sub_title("cloning from github")
+            emitter.normal("\t\tfetching code")
+            repo = Repo.clone_from(github_page, dir_src)
+            emitter.highlight(f"\t\t\t fetched dir: {dir_src}")
+            tag_list = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+            emitter.normal("\t\tfinding release tag")
+            release_tag = None
+            for t in tag_list:
+                if pkg_version.strip().lower() in str(t).strip().lower():
+                    release_tag = t
+                    break
+            emitter.highlight(f"\t\t\t release tag: {release_tag}")
+            repo.git.checkout(release_tag)
+    else:
+        emitter.normal("\t\tcache found, skipping fetch")
+
+
+def extract_meta_data(dir_pkg):
+    emitter.sub_title("Searching Meta Data")
+    emitter.sub_sub_title("finding for meta-data files")
+    meta_data_files = ["meta.yaml", "METADATA", "about.json", "index.json", "PKG-INFO"]
+    source_url = None
+    package_version = None
+    package_name = None
+    github_page = None
+    for f_name in meta_data_files:
+        result_list = utilities.find_file(dir_pkg, f_name)
+        if result_list:
+            for f_path in result_list:
+                if not f_path:
+                    continue
+                emitter.normal(f"\t\t found {f_path}")
+                abs_path = join(dir_pkg, f_path)
+                if "yaml" in f_name:
+                    meta_data = utilities.read_yaml(abs_path)
+                    meta_data_file = f_path
+                    if "source" in meta_data:
+                        source_url = meta_data["source"]["url"]
+                    if "package" in meta_data:
+                        package_name = meta_data["package"]["name"].strip()
+                        package_version = meta_data["package"]["version"].strip()
+                elif "json" in f_name:
+                    meta_data = utilities.read_json(abs_path)
+                    meta_data_file = f_path
+                    if "home" in meta_data:
+                        home_url = meta_data["home"]
+                        if "github.com" in home_url:
+                            github_page = home_url
+                    if "version" in meta_data:
+                        package_version = meta_data["version"].strip()
+                    if "name" in meta_data:
+                        package_name = meta_data["name"]
+                elif "PKG-INFO" in f_name:
+                    meta_info = utilities.read_file(abs_path)
+                    for l in meta_info:
+                        if "Name: " in l:
+                            package_name = l.split(": ")[-1].strip()
+                        elif "Version: " in l:
+                            package_version = l.split(": ")[-1].strip()
+                        elif "Home-page:" in l:
+                            home_url = l.split(": ")[-1]
+                            if "github.com" in home_url:
+                                github_page = home_url
+                                break
+                        elif "github.com" in l:
+                            result = re.search(r"//(.*?).git", l)
+                            if result:
+                                github_repo = result.group(1)
+                                github_page = f"https://{github_repo}.git"
+                                break
+                else:
+                    meta_info = utilities.read_file(abs_path)
+                    for l in meta_info:
+                        if "Name: " in l:
+                            package_name = l.split(": ")[-1].strip()
+                        elif "Version: " in l:
+                            package_version = l.split(": ")[-1].strip()
+                        elif "Home-page:" in l:
+                            home_url = l.split(": ")[-1]
+                            if "github.com" in home_url:
+                                github_page = home_url
+                                break
+
+            if source_url and package_version and package_name:
+                break
+            if github_page and package_name and package_version:
+                break
+
+    if not source_url and not github_page:
+        utilities.error_exit("failed to identify source")
+
+    emitter.highlight(f"\t\t\t package name: {package_name}")
+    emitter.highlight(f"\t\t\t package version: {package_version}")
+    emitter.highlight(f"\t\t\t package source: {source_url}")
+    emitter.highlight(f"\t\t\t package github: {github_page}")
+
+    return package_name, package_version, source_url, github_page
+
+
+def extract_archive(pkg_path):
+    emitter.sub_title("Extracting Files")
+    emitter.sub_sub_title("extracting original compressed file")
+    archive_path = str(pkg_path)
+    archive_name = str(archive_path).split("/")[-1]
+    file_extension = archive_name.split(".")[-1]
+    dir_pkg = f"{values.dir_experiments}/{archive_name}-dir"
+    extract_dir = archives.decompress_archive(archive_path, file_extension, dir_pkg)
+    emitter.sub_sub_title("extracting internally compressed file(s)")
+    archive_list = archives.find_compressed(extract_dir)
+    emitter.highlight(f"\t\t\tArchive File Count: {len(archive_list)}")
+    for a_f in archive_list:
+        archive_name = str(a_f).split("/")[-1]
+        file_extension = archive_name.split(".")[-1]
+        d_path = f"{a_f}-dir"
+        archives.decompress_archive(str(a_f), file_extension, d_path)
+    return dir_pkg
--- app/core/main.py
+++ app/core/main.py
@@ -18,7 +18,7 @@ import yaml
 from rich import get_console
 
 from app.core import configuration
-from app.core import definitions
+from app.core import extract
 from app.core import decompile
 from app.core import emitter
 from app.core import logger
@@ -71,129 +71,10 @@ def bootstrap(arg_list: Namespace):
 
 def run(parsed_args):
     package_path = parsed_args.file
-    emitter.sub_title("Extracting Files")
-    emitter.sub_sub_title("extracting original compressed file")
-    archive_path = str(package_path.name)
-    archive_name = str(archive_path).split("/")[-1]
-    file_extension = archive_name.split(".")[-1]
-    dir_pkg = f"{values.dir_experiments}/{archive_name}-dir"
-    extract_dir = archives.decompress_archive(archive_path, file_extension, dir_pkg)
-    emitter.sub_sub_title("extracting internally compressed file(s)")
-    archive_list = archives.find_compressed(extract_dir)
-    emitter.highlight(f"\t\t\tArchive File Count: {len(archive_list)}")
-    for a_f in archive_list:
-        archive_name = str(a_f).split("/")[-1]
-        file_extension = archive_name.split(".")[-1]
-        d_path = f"{a_f}-dir"
-        archives.decompress_archive(str(a_f), file_extension, d_path)
-
-    emitter.sub_title("Searching Meta Data")
-    emitter.sub_sub_title("finding for meta-data files")
-    meta_data_files = ["meta.yaml", "METADATA", "about.json", "index.json", "PKG-INFO"]
-    source_url = None
-    package_version = None
-    package_name = None
-    github_page = None
-    for f_name in meta_data_files:
-        result_list = utilities.find_file(dir_pkg, f_name)
-        if result_list:
-            for f_path in result_list:
-                if not f_path:
-                    continue
-                emitter.normal(f"\t\t found {f_path}")
-                abs_path = join(dir_pkg, f_path)
-                if "yaml" in f_name:
-                    meta_data = utilities.read_yaml(abs_path)
-                    meta_data_file = f_path
-                    if "source" in meta_data:
-                        source_url = meta_data["source"]["url"]
-                    if "package" in meta_data:
-                        package_name = meta_data["package"]["name"].strip()
-                        package_version = meta_data["package"]["version"].strip()
-                elif "json" in f_name:
-                    meta_data = utilities.read_json(abs_path)
-                    meta_data_file = f_path
-                    if "home" in meta_data:
-                        home_url = meta_data["home"]
-                        if "github.com" in home_url:
-                            github_page = home_url
-                    if "version" in meta_data:
-                        package_version = meta_data["version"].strip()
-                    if "name" in meta_data:
-                        package_name = meta_data["name"]
-                elif "PKG-INFO" in f_name:
-                    meta_info = utilities.read_file(abs_path)
-                    for l in meta_info:
-                        if "Name: " in l:
-                            package_name = l.split(": ")[-1].strip()
-                        elif "Version: " in l:
-                            package_version = l.split(": ")[-1].strip()
-                        elif "Home-page:" in l:
-                            home_url = l.split(": ")[-1]
-                            if "github.com" in home_url:
-                                github_page = home_url
-                                break
-                        elif "github.com" in l:
-                            result = re.search(r"//(.*?).git", l)
-                            if result:
-                                github_repo = result.group(1)
-                                github_page = f"https://{github_repo}.git"
-                                break
-                else:
-                    meta_info = utilities.read_file(abs_path)
-                    for l in meta_info:
-                        if "Name: " in l:
-                            package_name = l.split(": ")[-1].strip()
-                        elif "Version: " in l:
-                            package_version = l.split(": ")[-1].strip()
-                        elif "Home-page:" in l:
-                            home_url = l.split(": ")[-1]
-                            if "github.com" in home_url:
-                                github_page = home_url
-                                break
-
-            if source_url and package_version and package_name:
-                break
-            if github_page and package_name and package_version:
-                break
-
-    if not source_url and not github_page:
-        utilities.error_exit("failed to identify source")
-
-    emitter.highlight(f"\t\t\t package name: {package_name}")
-    emitter.highlight(f"\t\t\t package version: {package_version}")
-    emitter.highlight(f"\t\t\t package source: {source_url}")
-    emitter.highlight(f"\t\t\t package github: {github_page}")
-
-    emitter.sub_title("Fetching Source")
+    dir_pkg = extract.extract_archive(package_path.name)
+    package_name, package_version, source_url, github_page = extract.extract_meta_data(dir_pkg)
     dir_src = dir_pkg.replace("-dir", "-src")
-    if not os.path.isdir(dir_src):
-        if source_url:
-            emitter.sub_sub_title("downloading from URL")
-            release_file = os.path.dirname(dir_src) + "/src-archive"
-            utilities.download_file(source_url, release_file)
-            file_type = filetype.guess(release_file)
-            if not file_type:
-                utilities.error_exit(f"unknown release file type {release_file}")
-            file_ext = file_type.extension
-            archives.decompress_archive(release_file, file_ext, dir_src)
-        elif github_page:
-            emitter.sub_sub_title("cloning from github")
-            emitter.normal("\t\tfetching code")
-            repo = Repo.clone_from(github_page, dir_src)
-            emitter.highlight(f"\t\t\t fetched dir: {dir_src}")
-            tag_list = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-            emitter.normal("\t\tfinding release tag")
-            release_tag = None
-            for t in tag_list:
-                if package_version.strip().lower() in str(t).strip().lower():
-                    release_tag = t
-                    break
-            emitter.highlight(f"\t\t\t release tag: {release_tag}")
-            repo.git.checkout(release_tag)
-    else:
-        emitter.normal("\t\tcache found, skipping fetch")
-
+    extract.extract_source(source_url, github_page, dir_src, package_version)
     analysis.analyze_file_types(dir_pkg, dir_src)
     decompile.decompile_python_files(dir_pkg, dir_src)
 
