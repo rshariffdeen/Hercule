84138cf4f6f8c49534f754c91fa41762b4cbcbdd
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-12-26 15:42:32 +0800

    add instructions for RQ4 and script for figure-4

--- experiments/ICSE25/docs/RQ4.md
+++ experiments/ICSE25/docs/RQ4.md
@@ -1,7 +1,11 @@
 # Replicating Results for RQ4
 
 ## Performance Analysis (Figure-4)
-
-
+Once completing the steps in [RQ1](RQ1.md), which will generate the experiment data, following script will generate graph in Figure-4
+Run the script using the command
+```bash
+python3 scripts/graphs/figure-4.py 
+```
+It will generate a file named `performance.html` which is an interactive webpage showing the graph
 
 
--- experiments/ICSE25/scripts/graphs/figure-4.py
+++ experiments/ICSE25/scripts/graphs/figure-4.py
@@ -1,5 +1,7 @@
 import plotly.graph_objects as go
 import plotly
+import os
+import csv
 
 array_y = {}
 ranking = {}
@@ -8,10 +10,20 @@ datasets = ["MalOSS", "BackStabber", "MalRegistry", "Popular", "Trusted"]
 
 fig = go.Figure()
 
+def read_csv(file_path: str):
+    csv_data = None
+    if os.path.isfile(file_path):
+        with open(file_path, newline="") as csv_file:
+            raw_data = csv.DictReader(csv_file)
+            csv_data = [x for x in raw_data]
+    return csv_data
 
 for d in datasets:
-    file_name = f"performance/{d.lower()}"
-    data = open(file_name, "r").readlines()
+    csv_file_path = f"/experiments/hercule-{d}.csv"
+    if not os.path.isfile(csv_file_path):
+        continue
+    csv_data = read_csv(csv_file_path)
+    data = [x["Duration"] for x in csv_data]
     sorted_data = sorted([float(t) for t in data])
     print(d, len(sorted_data))
     fig.add_trace(go.Violin(y=sorted_data,
@@ -28,4 +40,4 @@ fig.update_layout(yaxis_zeroline=False, font=dict(
 fig.show()
 fig.show()
 # html file
-plotly.offline.plot(fig, filename='time.html')
+plotly.offline.plot(fig, filename='performance.html')
