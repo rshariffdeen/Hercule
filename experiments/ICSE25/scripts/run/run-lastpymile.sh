9685e0e868de46bf2a79c08302deb5218a723817
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2025-01-01 18:02:48 +0800

    delete artifacts after scan

--- experiments/ICSE25/scripts/run/run-lastpymile.sh
+++ experiments/ICSE25/scripts/run/run-lastpymile.sh
@@ -1,8 +1,10 @@
 python3 scripts/tools/lastpymile.py /data/popular/ data-set/popular
 mv /opt/hercule/results /opt/hercule/lastpymile-popular
+rm -rf /opt/hercule/experiments/*
 cp /data/popular/lastpymile_result.csv /experiments/lastpymile-popular.csv
 
 python3 scripts/tools/lastpymile.py /data/trusted/ data-set/trusted
 mv /opt/hercule/results /opt/hercule/lastpymile-trusted
+rm -rf /opt/hercule/experiments/*
 cp /data/trusted/lastpymile_result.csv /experiments/lastpymile-trusted.csv
 
